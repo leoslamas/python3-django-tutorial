3da3aceec837463bf05c554352be94a38ebaf118
Leonardo Lamas <leoslamas@gmail.com>
2020-03-12 06:20:34 -0300

    Customize the admin change list

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -14,6 +16,10 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
